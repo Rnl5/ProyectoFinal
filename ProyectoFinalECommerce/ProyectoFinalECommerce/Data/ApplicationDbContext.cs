aa9ae48c263e0c66f4d75c3993c77300c8ab5f85
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-11-26 22:08:51 -0400

    Agregando el OnModelCreate

--- ProyectoFinalECommerce/ProyectoFinalECommerce/Data/ApplicationDbContext.cs
+++ ProyectoFinalECommerce/ProyectoFinalECommerce/Data/ApplicationDbContext.cs
@@ -1,9 +1,37 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProyectoFinalECommerce.Shared;
 
 namespace ProyectoFinalECommerce.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<ProyectoFinalECommerce.Shared.TelefonosClientes> TelefonosClientes { get; set; } = default!;
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TelefonosClientes>().HasData(new List<TelefonosClientes>()
+        {
+
+            new TelefonosClientes(){
+                TipoTelId = 1, TipoTelefono ="Telefono Local"
+            },
+
+            new TelefonosClientes(){
+                TipoTelId = 2, TipoTelefono = "Celular"
+            },
+
+            new TelefonosClientes(){
+                TipoTelId = 3, TipoTelefono = "Trabajo"
+            },
+
+            new TelefonosClientes(){
+                TipoTelId = 4, TipoTelefono ="Persona Auxiliar"
+            }
+        });
+        }
     }
 }
