75e12222200414c01ced75e9533510eeb4559dfd
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-11-26 22:08:34 -0400

    Agregando el BaseAddress

--- ProyectoFinalECommerce/ProyectoFinalECommerce.Client/Program.cs
+++ ProyectoFinalECommerce/ProyectoFinalECommerce.Client/Program.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ProyectoFinalECommerce.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
-
+builder.Services.AddScoped(http => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+});
 await builder.Build().RunAsync();
