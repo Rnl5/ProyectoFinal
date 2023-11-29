83434392f10da47b98170f62a3aeb524c501537b
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-11-28 23:03:12 -0400

    Agregando propiedades del usuario en el ApplicationUser

--- ProyectoFinalECommerce/ProyectoFinalECommerce/Data/ApplicationUser.cs
+++ ProyectoFinalECommerce/ProyectoFinalECommerce/Data/ApplicationUser.cs
@@ -1,10 +1,22 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 
 namespace ProyectoFinalECommerce.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [Key]
+        public int UsuarioId { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Apellido { get; set; } = string.Empty;
+
+        public DateTime? FechaNacimiento { get; set; } = DateTime.Now.AddYears(-20);
+
+        public string NumeroCedula { get; set; } = string.Empty;
     }
 
 }
