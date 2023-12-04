b7b9fd3caaaa98271b3ad75810e0bf633628098f
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-12-04 00:36:16 -0400

    Creando el modelo categoria

---
+++ Shared/Models/Categorias.cs
@@ -0,0 +1,22 @@
+﻿using ProyectoFinalECommerce.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalECommerce.Shared;
+
+public class Categorias
+{
+    [Key]
+    public int CategoriaId { get; set; }
+
+    public string Nombre { get; set; }
+
+    public string Url { get; set; }
+
+    public string Icon { get; set; }
+
+}
