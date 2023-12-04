efa8e061492c213fbf2f7c476f9b7d21890118ca
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-12-04 00:36:51 -0400

    Actualizando el modelo de productos

--- Shared/Models/Productos.cs
+++ Shared/Models/Productos.cs
@@ -1,12 +1,18 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+
 namespace ProyectoFinalECommerce.Shared;
 
 public class Productos
 {
     [Key]
     public int ProductoId {get; set;}
+
+    [ForeignKey("CategoriaId")]
+    public int CategoriaId { get; set;}
+
+
     [Required(ErrorMessage = "Este campo es nesesario")]
     public DateTime Fecha {get; set;}
     [Required(ErrorMessage = "Este campo es necesario")]
@@ -16,8 +22,12 @@ public class Productos
     [Required(ErrorMessage = "Este campo es necesario")]
     public double Precio { get; set; }
 
+    public double PrecioOriginal { get; set; }
+
     [Required(ErrorMessage = "Este campo es necesario")]
     public byte[]? Foto { get; set; }
 
+    public bool EsPublico { get; set; }
 
+    public bool EstaBorrado {  get; set; }
 }
