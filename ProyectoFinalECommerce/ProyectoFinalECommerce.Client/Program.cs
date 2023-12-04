9cc74386f53daffa4dba3a9f563c89b544c40f46
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-12-04 00:35:04 -0400

    inyectando los servicios

--- ProyectoFinalECommerce/ProyectoFinalECommerce.Client/Program.cs
+++ ProyectoFinalECommerce/ProyectoFinalECommerce.Client/Program.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ProyectoFinalECommerce.Client;
+using ProyectoFinalECommerce.Client.Services.ServicioCategoria;
+using ProyectoFinalECommerce.Client.Services.ServicioProducto;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped<IServicioProducto, ServicioProducto>();
+builder.Services.AddScoped<IServicioCategoria, ServicioCategoria>();
+
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
@@ -10,4 +16,5 @@ builder.Services.AddScoped(http => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
 });
+
 await builder.Build().RunAsync();
