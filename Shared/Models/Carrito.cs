767a8299877f5717d250ec818e896e5a15f35f25
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-12-06 09:04:52 -0400

    Creando el modelo Carrito y sus servicios

---
+++ Shared/Models/Carrito.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalECommerce.Shared;
+
+public class Carrito
+{
+    [Key]
+    public int CarritoId { get; set; }
+
+    public int ProductoId { get; set; }
+
+    [Required(ErrorMessage ="El campo {0} es obligatorio")]
+    public string Nombre { get; set; }
+
+    [Required(ErrorMessage = "El campo {0} es obligatorio")]
+    public double Precio { get; set; }
+
+    [Required(ErrorMessage = "El campo {0} es obligatorio")]
+    public int Cantidad {  get; set; }
+
+}
