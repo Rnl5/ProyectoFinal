6656546db28c492721eb98c5c7ee77568abeb074
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-12-06 20:03:57 -0400

    Updateando base de datos y agregando migraciones

---
+++ ProyectoFinalECommerce/ProyectoFinalECommerce/Migrations/20231206211534_Agregando los modelos del telefono.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFinalECommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agregandolosmodelosdeltelefono : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EsPublico",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "EstaBorrado",
+                table: "Productos");
+
+            migrationBuilder.CreateTable(
+                name: "UsuariosDetalle",
+                columns: table => new
+                {
+                    DetalleIdCliente = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    TipoTelId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefono = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsuariosDetalle", x => x.DetalleIdCliente);
+                    table.ForeignKey(
+                        name: "FK_UsuariosDetalle_AspNetUsers_Id",
+                        column: x => x.Id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuariosDetalle_Id",
+                table: "UsuariosDetalle",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsuariosDetalle");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EsPublico",
+                table: "Productos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EstaBorrado",
+                table: "Productos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
---
+++ ProyectoFinalECommerce/ProyectoFinalECommerce/Migrations/20231206212036_Agregando los modelos del telefono en el context.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ProyectoFinalECommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agregandolosmodelosdeltelefonoenelcontext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TelefonosClientes",
+                keyColumn: "TipoTelId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TelefonosClientes",
+                keyColumn: "TipoTelId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TelefonosClientes",
+                keyColumn: "TipoTelId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "TelefonosClientes",
+                keyColumn: "TipoTelId",
+                keyValue: 4);
+
+            migrationBuilder.CreateTable(
+                name: "TelefonosUsuario",
+                columns: table => new
+                {
+                    TipoTelId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TipoTel = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TelefonosUsuario", x => x.TipoTelId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TelefonosUsuario",
+                columns: new[] { "TipoTelId", "TipoTel" },
+                values: new object[,]
+                {
+                    { 1, "Telefono Local" },
+                    { 2, "Celular" },
+                    { 3, "Trabajo" },
+                    { 4, "Persona Auxiliar" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuariosDetalle_TipoTelId",
+                table: "UsuariosDetalle",
+                column: "TipoTelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsuariosDetalle_TelefonosUsuario_TipoTelId",
+                table: "UsuariosDetalle",
+                column: "TipoTelId",
+                principalTable: "TelefonosUsuario",
+                principalColumn: "TipoTelId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsuariosDetalle_TelefonosUsuario_TipoTelId",
+                table: "UsuariosDetalle");
+
+            migrationBuilder.DropTable(
+                name: "TelefonosUsuario");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UsuariosDetalle_TipoTelId",
+                table: "UsuariosDetalle");
+
+            migrationBuilder.InsertData(
+                table: "TelefonosClientes",
+                columns: new[] { "TipoTelId", "TipoTelefono" },
+                values: new object[,]
+                {
+                    { 1, "Telefono Local" },
+                    { 2, "Celular" },
+                    { 3, "Trabajo" },
+                    { 4, "Persona Auxiliar" }
+                });
+        }
+    }
+}
